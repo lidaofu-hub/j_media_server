eccc74f6fe9d41250d31f48d4ddd8171d5b96eac
lidaofu <lidf@ahtelit.com>
2024-06-25 19:31:44 +0800

    feat: 增加whip/whep方式

--- src/main/java/com/ldf/media/api/controller/WebRTCController.java
+++ src/main/java/com/ldf/media/api/controller/WebRTCController.java
@@ -1,5 +1,6 @@
 package com.ldf.media.api.controller;
 
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import com.aizuda.zlm4j.callback.IMKWebRtcGetAnwerSdpCallBack;
@@ -9,14 +10,14 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.async.DeferredResult;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static com.ldf.media.context.MediaServerContext.ZLM_API;
@@ -34,6 +35,12 @@ import static com.ldf.media.context.MediaServerContext.ZLM_API;
 public class WebRTCController {
     @Autowired
     private MediaServerConfig config;
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private HttpServletResponse response;
+    @Value("${server.port}")
+    private int port;
 
     /**
      * webrtc sdp协议交互
@@ -78,6 +85,73 @@ public class WebRTCController {
             ResponseEntity<String> response = new ResponseEntity<>(result.toString(), HttpStatus.OK);
             out.setResult(response);
         };
+    }
+
+
+    /**
+     * 推流sdp交互
+     *
+     * @param app
+     * @param stream
+     */
+    @PostMapping("/index/api/whip")
+    public void whip(String app, String stream, @RequestBody String offerSdp) {
+        //这里可以做鉴权
+        String authorization = request.getHeader("Authorization");
+        //webrtc使用的是udp,默认监听8000,不需要设置端口号
+        String rtcUrl = StrUtil.format("rtc://{}:{}/{}/{}", config.getRtc_host(), config.getRtc_port(), app, stream);
+        IMKWebRtcGetAnwerSdpCallBack imkWebRtcGetAnwerSdpCallBack = (pointer, sdp, s1) -> {
+            try {
+                response.setContentType("application/sdp");
+                //todo 如果是https请换为https
+                String location = StrUtil.format("http://{}:{}/index/api/delete_webrtc?id={}&token={}", config.getRtc_host(), port, "whip_" + stream, RandomUtil.randomString(8));
+                response.setHeader("Location", location);
+                response.setStatus(201);
+                response.getWriter().write(sdp);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+        ZLM_API.mk_webrtc_get_answer_sdp(null, imkWebRtcGetAnwerSdpCallBack, "push", offerSdp, rtcUrl);
+    }
+
+    /**
+     * 拉流sdp交互
+     *
+     * @param app
+     * @param stream
+     */
+    @PostMapping("/index/api/whep")
+    public void whep(String app, String stream, @RequestBody String offerSdp) {
+        String rtcUrl = StrUtil.format("rtc://{}:{}/{}/{}", config.getRtc_host(), config.getRtc_port(), app, stream);
+        IMKWebRtcGetAnwerSdpCallBack imkWebRtcGetAnwerSdpCallBack = (pointer, sdp, s1) -> {
+            try {
+                response.setContentType("application/sdp");
+                //todo 如果是https请换为https
+                String location = StrUtil.format("http://{}:{}/index/api/delete_webrtc?id={}&token={}", config.getRtc_host(), port, "whip_" + stream, RandomUtil.randomString(8));
+                response.setHeader("Location", location);
+                response.setStatus(201);
+                response.getWriter().write(sdp);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+        ZLM_API.mk_webrtc_get_answer_sdp(null, imkWebRtcGetAnwerSdpCallBack, "play", offerSdp, rtcUrl);
+    }
+
+    /**
+     * 删除webrtc
+     * 要显式终止会话，WHEP 播放器必须对初始HTTP POST的Location头字段中返回的资源URL执行HTTP DELETE请求。收到HTTP DELETE请求后，WHEP资源将被删除，并在媒体服务器上释放资源
+     *
+     * @param id
+     * @param token
+     * @throws IOException
+     */
+    @DeleteMapping("/index/api/delete_webrtc")
+    public void deleteWebrtc(String id, String token) throws IOException {
+        //todo 校验token后删除资源
+        response.setStatus(200);
+        response.getWriter().write("");
     }
 
 }
