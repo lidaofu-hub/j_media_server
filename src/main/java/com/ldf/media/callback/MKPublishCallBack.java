4668ab36cf383e8d5b9cd5fb5fe23215711ae2bb
lidaofu <lidf@ahtelit.com>
2024-11-22 15:49:10 +0800

    feat: 修改配置

--- src/main/java/com/ldf/media/callback/MKPublishCallBack.java
+++ src/main/java/com/ldf/media/callback/MKPublishCallBack.java
@@ -1,6 +1,7 @@
 package com.ldf.media.callback;
 
 import com.aizuda.zlm4j.structure.MK_INI;
+import com.ldf.media.config.MediaServerConfig;
 import com.ldf.media.context.MediaServerContext;
 import com.aizuda.zlm4j.callback.IMKPublishCallBack;
 import com.aizuda.zlm4j.structure.MK_MEDIA_INFO;
@@ -8,6 +9,7 @@ import com.aizuda.zlm4j.structure.MK_PUBLISH_AUTH_INVOKER;
 import com.aizuda.zlm4j.structure.MK_SOCK_INFO;
 import com.sun.jna.CallbackThreadInitializer;
 import com.sun.jna.Native;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static com.ldf.media.context.MediaServerContext.ZLM_API;
@@ -20,6 +22,8 @@ import static com.ldf.media.context.MediaServerContext.ZLM_API;
  **/
 @Component
 public class MKPublishCallBack implements IMKPublishCallBack {
+    @Autowired
+    private MediaServerConfig config;
 
     public MKPublishCallBack() {
         //回调使用同一个线程
@@ -41,16 +45,17 @@ public class MKPublishCallBack implements IMKPublishCallBack {
         //String param = ZLM_API.mk_media_info_get_params(url_info);
         //MediaServerContext.ZLM_API.mk_publish_auth_invoker_do(invoker,"",1,0);
         MK_INI option = ZLM_API.mk_ini_create();
-        ZLM_API.mk_ini_set_option_int(option, "enable_mp4", 0);
-        ZLM_API.mk_ini_set_option_int(option, "enable_audio", 0);
-        ZLM_API.mk_ini_set_option_int(option, "enable_fmp4",0);
-        ZLM_API.mk_ini_set_option_int(option, "enable_ts", 0);
-        ZLM_API.mk_ini_set_option_int(option, "enable_hls",1);
-        ZLM_API.mk_ini_set_option_int(option, "enable_rtsp", 1);
-        ZLM_API.mk_ini_set_option_int(option, "enable_rtmp", 1);
-        //ZLM_API.mk_ini_set_option_int(option, "auto_close", 0);
-        ZLM_API.mk_ini_set_option_int(option, "mp4_max_second", 360);
-        ZLM_API.mk_ini_set_option_int(option, "segNum", 0);
+        ZLM_API.mk_ini_set_option_int(option, "enable_mp4", config.getEnable_mp4());
+        ZLM_API.mk_ini_set_option_int(option, "enable_audio", config.getEnable_audio());
+        ZLM_API.mk_ini_set_option_int(option, "enable_fmp4",config.getEnable_fmp4());
+        ZLM_API.mk_ini_set_option_int(option, "enable_hls_fmp4",config.getEnable_hls_fmp4());
+        ZLM_API.mk_ini_set_option_int(option, "enable_ts", config.getEnable_ts());
+        ZLM_API.mk_ini_set_option_int(option, "enable_hls",config.getEnable_hls());
+        ZLM_API.mk_ini_set_option_int(option, "enable_rtsp", config.getEnable_rtsp());
+        ZLM_API.mk_ini_set_option_int(option, "enable_rtmp", config.getEnable_rtmp());
+        ZLM_API.mk_ini_set_option_int(option, "auto_close", config.getAuto_close());
+        ZLM_API.mk_ini_set_option_int(option, "mp4_max_second", config.getMp4_max_second());
+        ZLM_API.mk_ini_set_option_int(option, "segNum", config.getSegNum());
         //流名称替换
         //ZLM_API.mk_ini_set_option(option, "stream_replace", "test1");
         ZLM_API.mk_publish_auth_invoker_do2(invoker, "", option);
